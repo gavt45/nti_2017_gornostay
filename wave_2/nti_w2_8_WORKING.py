3b9ac7c5d74bfb5f73593ba6fea6d4d0c954adc6
gavt45 <gavt45@bk.ru>
2017-10-21 14:47:41 +0700

    Update nti_w2_8_WORKING.py

--- wave_2/nti_w2_8_WORKING.py
+++ wave_2/nti_w2_8_WORKING.py
@@ -2,17 +2,9 @@ n, k = list(map(int, input().split()))
 points = list(map(int, input().split()))
 
 def solve():
-	#print(n, k)
-	#print(points)
-
 	first = points[0]
 	last = points[-1]
 	dist = last - first
-	#print(dist)
-
-	# min_lenght = dist / (n - 1)
-	# min_lenght = dist / k
-	# print(min_lenght)
 
 	for count in range(k, 0, -1):
 		lenght = dist / count
