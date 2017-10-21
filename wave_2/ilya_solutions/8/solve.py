de6cce394f7382f80c3a9886482fe80757d4d2c5
Ilya Maslovskiy <i.maslovskiy@expasoft.ru>
2017-10-21 14:00:10 +0700

    Add solution for 8 task. Non tested

---
+++ wave_2/ilya_solutions/8/solve.py
@@ -0,0 +1,41 @@
+import sys
+
+
+def parse_data(data):
+	lines = data.split('\n')
+	n, k = list(map(int, lines[0].split()))
+	points = list(map(int, lines[1].split()))
+	return n, k, points
+
+
+def solve(data):
+	n, k, points = parse_data(data)
+	print(n, k)
+	print(points)
+
+	first = points[0]
+	last = points[-1]
+	dist = last - first
+	print(dist)
+
+	# min_lenght = dist / (n - 1)
+	# min_lenght = dist / k
+	# print(min_lenght)
+
+	for count in range(k, 0, -1):
+		lenght = dist / count
+		print(count, lenght)
+
+		if not lenght.is_integer():
+			continue
+
+		lenght = int(lenght)
+		good_points = set(range(first, last+1, lenght))
+		
+		if good_points.issubset(points):
+			print(lenght)
+			break
+
+
+data = sys.stdin.read()
+solve(data)
